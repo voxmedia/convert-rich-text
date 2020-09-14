625698a583dee6617a8c6cce3e64f7c8ab0f5bc6
Michele Cynowicz <michele.cynowicz@voxmedia.com>
2020-09-14 16:54:20 -0400

    delete all toInternalHtml methods

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var JSDOM = require('jsdom').JSDOM;
 var toHtml = require('./lib/export/to_html');
 var toInlineHtml = require('./lib/export/to_inline_html');
-var toInternalHtml = require('./lib/export/to_internal_html');
 var toPublicHtml = require('./lib/export/to_public_html');
 var toPlaintext = require('./lib/export/to_plaintext');
 
@@ -13,10 +12,6 @@ module.exports = {
   toInlineHtml: function(delta, formats, options) {
     options = Object.assign({}, options, { document: new JSDOM().window.document });
     return toInlineHtml(delta, formats, options);
-  },
-  toInternalHtml: function(delta, formats, options) {
-    options = Object.assign({}, options, { document: new JSDOM().window.document });
-    return toInternalHtml(delta, formats, options);
   },
   toPublicHtml: function(delta, formats, options) {
     options = Object.assign({}, options, { document: new JSDOM().window.document });
